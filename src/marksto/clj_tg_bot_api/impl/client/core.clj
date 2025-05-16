5ef9e58b444c4c75d8599e0d42240cbb0d3c9fae
Mark Sto <marksto@yourown.software>
2025-05-16 20:16:12 +0400

    Spread functionality between namespaces

--- src/marksto/clj_tg_bot_api/impl/client/core.clj
+++ src/marksto/clj_tg_bot_api/impl/client/core.clj
@@ -2,12 +2,11 @@
   (:require [camel-snake-kebab.core :as csk]
             [clojure.string :as str]
             [clojure.tools.logging :as log]
-            [diehard.core :as dh]
-            [diehard.rate-limiter :as dh.rl]
             [martian.core :as m]
             [taoensso.truss :refer [have!]]
 
             [marksto.clj-tg-bot-api.impl.api.spec :as api-spec]
+            [marksto.clj-tg-bot-api.impl.client.rate-limiter :as rl]
             [marksto.clj-tg-bot-api.impl.utils :as utils]
             [marksto.clj-tg-bot-api.impl.utils.response :as response]))
 
@@ -134,86 +133,6 @@
 
 ;;; Making Requests
 
-;; NB: The Bots FAQ on the official Telegram website lists the following limits
-;;     on server requests.
-;;
-;;     LEGACY VERSION
-;;     "When sending messages inside a particular chat, avoid sending more than
-;;      1 message per second. We may allow short bursts that go over this limit,
-;;      but eventually you'll begin receiving 429 errors.
-;;
-;;      Also note that your bot will not be able to send more than 20 messages
-;;      per minute to the same group.
-;;
-;;      If you're sending bulk notifications to multiple users, the API will not
-;;      allow more than 30 messages per second or so. Consider spreading out
-;;      notifications over large intervals of 8—12 hours for best results."
-;;
-;;     CURRENT VERSION
-;;     "By default, bots are able to message their users *at no cost* – but have
-;;      limitations on the number of messages they can broadcast in a single
-;;      interval:
-;;
-;;      - In a single chat, avoid sending more than one message per second. We
-;;        may allow short bursts that go over this limit, but eventually you'll
-;;        begin receiving 429 errors.
-;;
-;;      - In a group, bots aren't able to send more than 20 messages per minute.
-;;
-;;      - For bulk notifications, bots are not able to broadcast more than about
-;;        30 messages per second, unless they enable paid broadcasts to increase
-;;        the limit."
-;;
-;;     See details in https://core.telegram.org/bots/faq#broadcasting-to-users,
-;;     "My bot is hitting limits, how do I avoid this?".
-;;
-;;     IMPLEMENTATION
-;;
-;;     To overcome the issue with HTTP 429 — "Too many requests, retry after X"
-;;     error, the Telegram Bot API client implements the following rate limits:
-;;
-;;     - No more than 1  message  per second in a single chat,
-;;     - No more than 20 messages per minute in the same group chat,
-;;     - No more than 30 messages per second in total (for broadcasting).
-
-(defonce
-  ^{:doc "An atom that holds a map of the following structure:
-          {<bot-id> {:total    <total bot rate limiter>
-                     <chat-id> <some chat rate limiter>}}"}
-  *rate-limiters
-  (atom {}))
-
-(defn ->rate [n sec]
-  (/ (double n) sec))
-
-(defn ->total-rate-limiter []
-  (dh.rl/rate-limiter {:rate (->rate 30 1)}))
-
-(defn ->chat-rate-limiter [chat-id]
-  (if-let [_is-group? (neg? chat-id)]
-    (dh.rl/rate-limiter {:rate (->rate 20 60)})
-    (dh.rl/rate-limiter {:rate (->rate 1 1)})))
-
-(defn- get-rate-limiter!
-  ([bot-id]
-   (-> *rate-limiters
-       (swap!
-         (fn [rate-limiters]
-           (if (some? (get-in rate-limiters [bot-id :total]))
-             rate-limiters
-             (assoc-in rate-limiters [bot-id :total] (->total-rate-limiter)))))
-       (get-in [bot-id :total])))
-  ([bot-id chat-id]
-   (-> *rate-limiters
-       (swap!
-         (fn [rate-limiters]
-           (if (some? (get-in rate-limiters [bot-id chat-id]))
-             rate-limiters
-             (assoc-in rate-limiters [bot-id chat-id] (->chat-rate-limiter chat-id)))))
-       (get-in [bot-id chat-id]))))
-
-;;
-
 (defn- call-tg-bot-api-method!
   [client method params]
   ;; TODO: Double-check with all popular clients and re-implement if necessary.
@@ -228,16 +147,6 @@
     (catch Throwable client-code-ex
       ;; NB: An `error` is processed by the `handle-response` function.
       {:error client-code-ex})))
-
-(defmacro with-rate-limiter
-  [bot-id in-test? chat-id form]
-  `(if ~in-test?
-     ~form
-     (dh/with-rate-limiter (get-rate-limiter! ~bot-id)
-       (if (some? ~chat-id)
-         (dh/with-rate-limiter (get-rate-limiter! ~bot-id ~chat-id)
-           ~form)
-         ~form))))
 
 ;;
 
@@ -280,7 +189,7 @@
                      :on-error   (or (:on-error call-opts)
                                      log-error-and-rethrow)}
           chat-id (or (get params :chat-id) (get params :chat_id))
-          {:keys [body]} (with-rate-limiter bot-id in-test? chat-id
+          {:keys [body]} (rl/with-rate-limiter bot-id in-test? chat-id
                            (call-tg-bot-api-method! client method params))]
       (log/debugf "Telegram Bot API returned: %s" body)
       (when (some? body)
--- src/marksto/clj_tg_bot_api/impl/client/rate_limiter.clj
+++ src/marksto/clj_tg_bot_api/impl/client/rate_limiter.clj
@@ -1,138 +1,6 @@
-(ns marksto.clj-tg-bot-api.impl.client.core
-  (:require [camel-snake-kebab.core :as csk]
-            [clojure.string :as str]
-            [clojure.tools.logging :as log]
-            [diehard.core :as dh]
-            [diehard.rate-limiter :as dh.rl]
-            [martian.core :as m]
-            [taoensso.truss :refer [have!]]
-
-            [marksto.clj-tg-bot-api.impl.api.spec :as api-spec]
-            [marksto.clj-tg-bot-api.impl.utils :as utils]
-            [marksto.clj-tg-bot-api.impl.utils.response :as response]))
-
-;;; Bot API client
-
-(def global-server-url "https://api.telegram.org/bot")
-
-(defn get-api-root-url-for-bot
-  [server-url bot-auth-token]
-  (str (or server-url global-server-url) bot-auth-token))
-
-;; TODO: Hide behind a custom `:type` to prevent secrets (token) from leaking.
-(defn ->client
-  [{:keys [bot-id bot-auth-token server-url] :as _client-opts}]
-  (-> (get-api-root-url-for-bot server-url bot-auth-token)
-      (api-spec/build-martian)
-      (assoc :bot-id bot-id)))
-
-
-;;; Operations on response 'result'
-
-(defn get-result
-  ([tg-resp]
-   (get-result tg-resp nil))
-  ([tg-resp default-val]
-   (or (response/get-response-result tg-resp) default-val)))
-
-(defn assert-result
-  [expected tg-resp]
-  (let [tg-resp-result (get-result tg-resp)]
-    (when-not (= expected tg-resp-result)
-      (throw
-        (ex-info "The Telegram Bot API method returned an unexpected result"
-                 {:expected expected
-                  :actual   tg-resp-result})))
-    tg-resp-result))
-
-
-;;; Operations on response 'error'
-
-(defn get-error
-  [method params failed-tg-resp]
-  (let [resp-error (response/get-response-error failed-tg-resp)]
-    (assoc resp-error :method method :params params)))
-
-(def failure-msg "Unsuccessful Telegram Bot API request")
-
-(defn- format-msg [base-msg method-name params]
-  (format "%s (method='%s' args=%s)" base-msg method-name params))
-
-(defn log-failure-reason
-  ([method params failed-tg-resp]
-   (log-failure-reason :error failure-msg
-                       method params failed-tg-resp))
-  ([base-msg method params failed-tg-resp]
-   (log-failure-reason :error base-msg
-                       method params failed-tg-resp))
-  ([log-level base-msg method params failed-tg-resp]
-   (let [resp-error (response/get-response-error failed-tg-resp)
-         error-text (some->> (seq resp-error)
-                             (map (fn [[k v]] (str (name k) "=\"" v "\"")))
-                             (not-empty)
-                             (str/join ", "))
-         base-msg' (cond-> (utils/apply-if-fn base-msg)
-                           (seq error-text) (str ": " error-text))
-         method-name (utils/fn-name method)]
-     (log/log log-level (format-msg base-msg' method-name params)))))
-
-(defn throw-for-failure
-  ([method params failed-tg-resp]
-   (throw-for-failure failure-msg method params failed-tg-resp))
-  ([base-msg method params failed-tg-resp]
-   (let [ex-msg (utils/apply-if-fn base-msg)
-         ;; NB: Exclude an HTTP client-set `:error`, since it's of little use.
-         response (dissoc failed-tg-resp :error)]
-     (throw (ex-info ex-msg {:response response
-                             :method   method
-                             :params   params})))))
-
-;; NB: For async requests, this strategy is of little use, since the provided
-;;     callback will be processed on different threads and any exception will
-;;     be redirected to the `UncaughtExceptionHandler` which will simply log.
-(defn log-failure-reason-and-throw
-  ([method params failed-tg-resp]
-   (log-failure-reason method params failed-tg-resp)
-   (throw-for-failure method params failed-tg-resp))
-  ([base-msg method params failed-tg-resp]
-   (log-failure-reason base-msg method params failed-tg-resp)
-   (throw-for-failure base-msg method params failed-tg-resp))
-  ([log-level base-msg method params failed-tg-resp]
-   (log-failure-reason log-level base-msg method params failed-tg-resp)
-   (throw-for-failure base-msg method params failed-tg-resp)))
-
-
-;;; Operations on arbitrary errors (e.g. network)
-
-(def error-msg "Error while making a Telegram Bot API request")
-
-(defn log-error
-  ([method params ex]
-   (log-error error-msg method params ex))
-  ([base-msg method params ex]
-   (let [base-msg' (utils/apply-if-fn base-msg)
-         method-name (utils/fn-name method)]
-     (log/log :error ex (format-msg base-msg' method-name params)))))
-
-;; NB: For async requests, this strategy is of little use, since the provided
-;;     callback will be processed on different threads and any exception will
-;;     be redirected to the `UncaughtExceptionHandler` which will simply log.
-(defn rethrow-error
-  [method params ex]
-  (throw (ex-info error-msg {:method method :params params} ex)))
-
-(defn log-error-and-rethrow
-  ([method params ex]
-   (log-error method params ex)
-   (when-not (utils/in-repl?) (utils/clear-stack-trace ex))
-   (rethrow-error method params ex))
-  ([base-msg method params ex]
-   (log-error base-msg method params ex)
-   (when-not (utils/in-repl?) (utils/clear-stack-trace ex))
-   (rethrow-error method params ex)))
-
-
-;;; Making Requests
+(ns marksto.clj-tg-bot-api.impl.client.rate-limiter
+  (:require [diehard.core :as dh]
+            [diehard.rate-limiter :as dh.rl]))
 
 ;; NB: The Bots FAQ on the official Telegram website lists the following limits
 ;;     on server requests.
@@ -194,7 +62,7 @@
     (dh.rl/rate-limiter {:rate (->rate 20 60)})
     (dh.rl/rate-limiter {:rate (->rate 1 1)})))
 
-(defn- get-rate-limiter!
+(defn get-rate-limiter!
   ([bot-id]
    (-> *rate-limiters
        (swap!
@@ -214,21 +82,6 @@
 
 ;;
 
-(defn- call-tg-bot-api-method!
-  [client method params]
-  ;; TODO: Double-check with all popular clients and re-implement if necessary.
-  ;; NB: Expects an HTTP client library to return a map with the `:error` key
-  ;;     in case if request was unsuccessful (status codes other than 200-207,
-  ;;     300-303, or 307) or in any of other exceptional situations (e.g. when
-  ;;     the lib is unable to retrieve the response body).
-  (try
-    (if (nil? params)
-      (m/response-for client method)
-      (m/response-for client method params))
-    (catch Throwable client-code-ex
-      ;; NB: An `error` is processed by the `handle-response` function.
-      {:error client-code-ex})))
-
 (defmacro with-rate-limiter
   [bot-id in-test? chat-id form]
   `(if ~in-test?
@@ -238,84 +91,3 @@
          (dh/with-rate-limiter (get-rate-limiter! ~bot-id ~chat-id)
            ~form)
          ~form))))
-
-;;
-
-(defn- call-ignorable-callback
-  [callback-fn method params arg-val]
-  (when (and (some? callback-fn) (not= :ignore callback-fn))
-    (callback-fn method params arg-val)))
-
-(defn- handle-response
-  [method params tg-resp {:keys [on-success on-failure on-error] :as _callbacks}]
-  ;; NB: The order of checks here is crucial. First, we handle valid responses,
-  ;;     including unsuccessful ones (also contain `:error` key). Only then we
-  ;;     handle other `:error`-containing tg-resp, e.g. HTTP, response parsing
-  ;;     and client code exceptions.
-  (cond
-    (response/valid-response? tg-resp)
-    (if (response/successful-response? tg-resp)
-      (when (some? on-success) (on-success tg-resp))
-      (call-ignorable-callback on-failure method params tg-resp))
-    ;;
-    (contains? tg-resp :error)
-    (call-ignorable-callback on-error method params (:error tg-resp))
-    ;;
-    :else
-    (throw (IllegalStateException. "Malformed Telegram Bot API response"))))
-
-;;
-
-;; TODO: Add an auto-retry strategy for Telegram Bot API response 'parameters' with 'migrate_to_chat_id'.
-;; TODO: Add an auto-retry strategy for Telegram Bot API response 'parameters' with 'retry_after'.
-
-(defn make-request!
-  [{:keys [bot-id in-test?] :as client} args]
-  (let [[call-opts method params] (if (map? (first args)) args (cons nil args))]
-    (have! keyword? method)
-    (let [callbacks {:on-success (or (:on-success call-opts)
-                                     get-result)
-                     :on-failure (or (:on-failure call-opts)
-                                     log-failure-reason-and-throw)
-                     :on-error   (or (:on-error call-opts)
-                                     log-error-and-rethrow)}
-          chat-id (or (get params :chat-id) (get params :chat_id))
-          {:keys [body]} (with-rate-limiter bot-id in-test? chat-id
-                           (call-tg-bot-api-method! client method params))]
-      (log/debugf "Telegram Bot API returned: %s" body)
-      (when (some? body)
-        (handle-response method params body callbacks)))))
-
-;;
-
-(defn build-immediate-response
-  ([client method]
-   (build-immediate-response client method {}))
-  ([client method params]
-   (have! keyword? method)
-   (let [request (-> client
-                     (m/request-for method params)
-                     (select-keys [:body :multipart :headers])
-                     (assoc :status 200))
-         method' (csk/->camelCaseString method)]
-     ;; NB: Special handling of multipart requests so to not pollute the params
-     ;;     schema of each and every method in the API spec with this `method`.
-     (if (contains? request :multipart)
-       (update request :multipart conj {:name "method" :content method'})
-       (update request :body assoc :method method')))))
-
-;;
-
-(comment
-  (def client (->client {:bot-id         1
-                         :bot-auth-token (System/getenv "BOT_AUTH_TOKEN")}))
-  (dissoc client :handlers)
-
-  (make-request! client '(:get-me))
-  (make-request! client '(:get-me {}))
-
-  (build-immediate-response client :get-me)
-  (build-immediate-response client :get-me {})
-  (build-immediate-response client :send-audio {:chat-id 1 :audio "<audio>"})
-
-  :end/comment)
